174b5890a2d14494ad73b382fc651b424e7422bc
Borja Lazaro <blazaro@autentia.com>
2017-04-01 17:05:40 +0200

    Mensaje de intercambio entre la tienda y la cocina.

--- src/main/java/com/autentia/workshop/akka/practice/model/TortillaOrder.java
+++ src/main/java/com/autentia/workshop/akka/practice/model/TortillaOrder.java
@@ -1,7 +1,28 @@
 package com.autentia.workshop.akka.practice.model;
 
+import com.autentia.workshop.tortilla.*;
+
+import java.io.Serializable;
+
 /**
  * Created by blazaro on 1/4/17.
  */
-public class TortillaOrder {
+public class TortillaOrder implements Serializable{
+
+    private final TortillaType tortillaType;
+    private final Onions onions;
+    private final OliveOil oliveOil;
+    private final Potatoes potatoes;
+    private final Eggs eggs;
+    private final Salt salt;
+
+    public TortillaOrder(TortillaType tortillaType, Onions onions, OliveOil oliveOil, Potatoes potatoes, Eggs eggs, Salt salt){
+
+        this.tortillaType = tortillaType;
+        this.onions = onions;
+        this.oliveOil = oliveOil;
+        this.potatoes = potatoes;
+        this.eggs = eggs;
+        this.salt = salt;
+    }
 }
